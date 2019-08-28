470648250eb1d1bda99d7ecb3b9cdc3453a78fdb
Julien Monchany <julien.monchany@gmail.com>
2019-08-29 00:24:36 +0200

    Cart test

--- src/components/Cart/__tests__/Cart.js
+++ src/components/Cart/__tests__/Cart.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect'
-
 import {render, fireEvent} from '@testing-library/react';
+
 import { Cart } from '../Cart';
 
-test("render an empty cart", () => {
-  const { container, getByText } = render(<Cart />);
+it("render an empty cart", () => {
+  const { getByText } = render(<Cart flushCart={jest.fn()} />);
   expect(getByText("Total: 0 €")).toBeInTheDocument();
-  expect(container.querySelector("p b").textContent).toEqual("Total: 0 €");
+});
+
+it("trigger flush cart function", () => {
+  const mockFlushCart = jest.fn();
+  const { getByText } = render(<Cart flushCart={mockFlushCart} />);
+    // Click event check
+    fireEvent.click(getByText("Vider le panier"));
+    expect(mockFlushCart).toHaveBeenCalledTimes(1);
+})
+
+it("render an existing cart", () => {
+  const fakeCart = [
+    {name: "Pizza 1", price: 10},
+    {name: "Pizza 2", price: 25}
+  ]
+  const { getByText, getAllByText } = render(<Cart cart={fakeCart} flushCart={jest.fn()} />);
+  
+  const displayedPizzas = getAllByText(/pizza/i);
+  expect(displayedPizzas.length).toEqual(2);
+  displayedPizzas.forEach((pizzaNode, i) => {
+    expect(pizzaNode.textContent).toBe(fakeCart[i].name);
+  })
+  
+  expect(getByText("Total: 35 €")).toBeInTheDocument();
 });
