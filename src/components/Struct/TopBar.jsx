1b1e053df520eeb390b6e74063fedd784afc4b32
Julien Monchany <julien.monchany@gmail.com>
2019-08-30 10:30:52 +0200

    React Hook example

--- src/components/Struct/TopBar.jsx
+++ src/components/Struct/TopBar.jsx
@@ -2,67 +2,58 @@ import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/Pizza-Yolo.png";
 
-export default class TopBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isMenuOpen: false
-    };
-  }
+/**
+ * TopBar Component using React useState hook
+ * @see file TopBarOldSchool.jsx to see the classic state management
+ */
+export function TopBar() {
 
-  /**
-   * On mobile: toggle the menu
-   */
-  onMenuBtnClick = () => {
-    this.setState(state => ({
-      isMenuOpen: !state.isMenuOpen
-    }));
-  };
+  // define a value for isMenuOpen and declare the update func
+  const [isMenuOpen, displayMenu ] = useState(false);
 
-  render() {
-    const { isMenuOpen } = this.state;
-    return (
-      <nav className="is-primary navbar">
-        <div className="navbar-brand">
-          <NavLink to="/" className="navbar-item">
-            <img src={logo} alt="PizzaYolo logo" />
-          </NavLink>
-          <button
-            className={`navbar-burger${isMenuOpen ? " is-active" : ""}`}
-            data-target="navMenu"
-            aria-label="menu"
-            aria-expanded="false"
-            onClick={this.onMenuBtnClick}
-          >
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-            <span aria-hidden="true" />
-          </button>
-        </div>
-
-        <div
-          id="navMenu"
-          className={`navbar-menu${isMenuOpen ? " is-active" : ""}`}
+  return (
+    <nav className="is-primary navbar">
+      <div className="navbar-brand">
+        <NavLink to="/" className="navbar-item">
+          <img src={logo} alt="PizzaYolo logo" />
+        </NavLink>
+        <button
+          className={`navbar-burger${isMenuOpen ? " is-active" : ""}`}
+          data-target="navMenu"
+          aria-label="menu"
+          aria-expanded="false"
+          onClick={() => displayMenu(!isMenuOpen)}
         >
-          <div className="navbar-start">
-            <NavLink
-              to="/"
-              exact
-              className="navbar-item"
-              activeClassName="selected"
-            >
-              Accueil
-            </NavLink>
-            <NavLink
-              to="/pizzas"
-              className="navbar-item"
-              activeClassName="selected"
-            >
-              Nos Pizzas
-            </NavLink>
-          </div>
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </button>
+      </div>
+
+      <div
+        id="navMenu"
+        className={`navbar-menu${isMenuOpen ? " is-active" : ""}`}
+      >
+        <div className="navbar-start">
+          <NavLink
+            to="/"
+            exact
+            className="navbar-item"
+            activeClassName="selected"
+          >
+            Accueil
+          </NavLink>
+          <NavLink
+            to="/pizzas"
+            className="navbar-item"
+            activeClassName="selected"
+          >
+            Nos Pizzas
+          </NavLink>
         </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
+
+export default TopBar;
